a6634e0c48f79fa24d737d6e03287aabb7eefa3f
Erik <flyers_rock_87@hotmail.com>
2019-03-19 18:03:51 +0000

    separate get_csv_data method

--- app/models/sp_market_cap.rb
+++ app/models/sp_market_cap.rb
@@ -1,35 +1,35 @@
 class SpMarketCap < ActiveRecord::Base
-  def self.get_mkt_caps
+  def self.get_csv_data(date)
     require 'open-uri'
     require 'csv'
     
-    def get_csv_data(date)
-      i = 0
-      as_of = ""
-      
-      while i < 10 && as_of.blank?
-        # loop back through dates until date includes data
-        base_url = "https://www.blackrock.com/us/individual/products/239726/ishares-core-sp-500-etf/1464253357814.ajax?fileType=csv&fileName=IVV_holdings&dataType=fund&asOfDate="
-        
-        # set url
-        d = date.strftime("%Y%m%d")
-        url = base_url + d
-        
-        # get csv data
-        url_data = open(url).read()
-        csv = CSV.parse(url_data)
-        
-        as_of = csv.find { |row| row[0] == "Fund Holdings as of" }[1]
-        
-        # increment down if as_of not resolved
-        if as_of.blank? 
-          date = date - 1.day
-        end
-      end
-        
-      return csv, date
-    end
+    i = 0
+    as_of = ""
     
+    while i < 10 && as_of.blank?
+      # loop back through dates until date includes data
+      base_url = "https://www.blackrock.com/us/individual/products/239726/ishares-core-sp-500-etf/1464253357814.ajax?fileType=csv&fileName=IVV_holdings&dataType=fund&asOfDate="
+      
+      # set url
+      d = date.strftime("%Y%m%d")
+      url = base_url + d
+      
+      # get csv data
+      url_data = open(url).read()
+      csv = CSV.parse(url_data)
+      
+      as_of = csv.find { |row| row[0] == "Fund Holdings as of" }[1]
+      
+      # increment down if as_of not resolved
+      if as_of.blank? 
+        date = date - 1.day
+      end
+    end
+      
+    return csv, date
+  end
+  
+  def self.get_mkt_caps
     start_date = SpMarketCap.all.map(&:date).max || Date.new(2006,9,29)
 
     # start with end months
@@ -38,7 +38,7 @@ class SpMarketCap < ActiveRecord::Base
       puts "Getting market cap for #{d.strftime("%F")}..."
       
       # process data
-      csv, date = get_csv_data(d)
+      csv, date = SpMarketCap.get_csv_data(d)
       mkt_cap = csv.find { |row| row[0] == "Total Net Assets"}
       
       # create entry for this month
